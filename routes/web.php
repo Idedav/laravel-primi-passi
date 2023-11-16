4ab38f02053b5ccfabe5128487d7d532187196f2
Idedav <xdavide.correa@gmail.com>
2023-11-16 14:35:00 +0100

    [add] home page

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $name = 'Davide';
+    $lastname = 'Correa';
+    return view('home', compact('name', 'lastname'));
 });
